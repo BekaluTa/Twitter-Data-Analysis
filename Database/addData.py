8d7f12a6216942a9d1a8732b7c0a9ddf82e10d0f
BekaluTa <bekiman21@gmail.com>
2022-08-12 17:21:51 +0300

    Database and Tbale Creation

--- Database/addData.py
+++ Database/addData.py
@@ -17,8 +17,7 @@ class DBoperations:
 
     def DBConnect(self, dbName=None):
         try:
-            # engine = create_engine(
-            #     'postgresql+psycopg2://postgres:password@localhost/twitter')
+        # creating Datbase Connection
             conn = psycopg2.connect(
                 database=self.database, user=self.user, password=self.password, host=self.host, port=self.port
             )
@@ -39,17 +38,6 @@ class DBoperations:
         conn.commit()
 
     def createDB(self) -> None:
-        """
-        Parameters
-        ----------
-        dbName :
-            str:
-        dbName :
-            str:
-        dbName:str :
-        Returns
-        -------
-        """
         conn, cur = DBoperations.DBConnect(self)
         # cur.execute(f"CREATE DATABASE IF NOT EXISTS {dbName};")
         return conn
@@ -57,17 +45,6 @@ class DBoperations:
         # cur.close()
 
     def createTables(self, dbName: str) -> None:
-        """
-        Parameters
-        ----------
-        dbName :
-            str:
-        dbName :
-            str:
-        dbName:str :
-        Returns
-        -------
-        """
         conn, cur = DBoperations.DBConnect(self, 'tweets')
         sqlFile = '../schema.sql'
         fd = open(sqlFile, 'r')
@@ -88,17 +65,6 @@ class DBoperations:
         return
 
     def preprocess_df(self, df: pd.DataFrame) -> pd.DataFrame:
-        """
-        Parameters
-        ----------
-        df :
-            pd.DataFrame:
-        df :
-            pd.DataFrame:
-        df:pd.DataFrame :
-        Returns
-        -------
-        """
         cols_2_drop = ['Unnamed: 0', 'possibly_sensitive']
         try:
             df = df.drop(columns=cols_2_drop, axis=1)
@@ -109,27 +75,6 @@ class DBoperations:
         return df
 
     def insert_to_tweet_table(self, dbName: str, df: pd.DataFrame, table_name: str) -> None:
-        """
-        Parameters
-        ----------
-        dbName :
-            str:
-        df :
-            pd.DataFrame:
-        table_name :
-            str:
-        dbName :
-            str:
-        df :
-            pd.DataFrame:
-        table_name :
-            str:
-        dbName:str :
-        df:pd.DataFrame :
-        table_name:str :
-        Returns
-        -------
-        """
         conn, cur = DBoperations.DBConnect(self, 'tweets')
 
         df = DBoperations.preprocess_df(self, df)
@@ -159,20 +104,7 @@ class DBoperations:
         return
 
     def db_execute_fetch(self, *args, many=False, tablename='', rdf=True, **kwargs) -> pd.DataFrame:
-        """
-        Parameters
-        ----------
-        *args :
-        many :
-            (Default value = False)
-        tablename :
-            (Default value = '')
-        rdf :
-            (Default value = True)
-        **kwargs :
-        Returns
-        -------
-        """
+      
         connection, cursor1 = DBoperations.DBConnect(self, 'tweets')
         if many:
             cursor1.executemany(*args)
@@ -203,29 +135,6 @@ class DBoperations:
 st.set_page_config(page_title="Dashboard", layout="wide")
 
 
-# def app():
-# try:
-#     engine = create_engine(
-#         'postgresql+psycopg2://postgres:password@localhost/twitter')
-# except exc.SQLAlchemyError as e:
-#     print("Error", e)
-
-# st.title('Data')
-
-# st.write("This is the `Data` page of the multi-page app.")
-
-# st.write("The following is the DataFrame of the `iris` dataset.")
-
-# iris = datasets.load_iris()
-# X = pd.DataFrame(iris.data, columns=iris.feature_names)
-# Y = pd.Series(iris.target, name='class')
-# df = pd.concat([X, Y], axis=1)
-# df['class'] = df['class'].map(
-#     {0: "setosa", 1: "versicolor", 2: "virginica"})
-
-# st.write(df)
-
-
 if __name__ == '__main__':
     host = config('Host', default='')
     database = config('Database', default='')
@@ -234,24 +143,6 @@ if __name__ == '__main__':
     port = config('Port', default='')
     db1 = DBoperations(host, database, user, password, port)
     db1.createDB()
-    # # db1.emojiDB('tweets')
-
-    # db1.createTables('twitter')
 
     df = pd.read_csv('../clean_tweet.csv')[6000:9500]
     db1.insert_to_tweet_table('tweets', df=df, table_name='tweetinformation')
-
-    # # obj1 = st.disp()
-    # st.markdown("<p style='padding:30px;text-align:center; background-color:#000000;color:#00ECB9;font-size:26px;border-radius:10px;'>Twitter Data Analysis Dashboard</p>", unsafe_allow_html=True)
-    # st.title("Data Display")
-    # # obj1.selectHashTag()
-    # st.markdown("<p style='text-align:center;padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Section Break</p>", unsafe_allow_html=True)
-    # st.title("Data Visualizations")
-
-    # obj1.wordCloud()
-    # with st.beta_expander("Show More Graphs"):
-    #     obj1.stBarChart()
-    #     obj1.stBarChart2()
-    #     obj1.stBarChart3()
-    #     obj1.Text_catagory_Pie()
-    #     obj1.langPie()
